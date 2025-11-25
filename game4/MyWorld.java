d0e06f16e8405b43c4475469c826791239fac642
nasubi1108 <21097@hakodate.kosen-ac.jp>
2025-11-25 10:26:13 +0900

    画像とタイマーの追加

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -6,16 +6,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+
 public class MyWorld extends World
 {
-
+    private int timecount = 1000;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        GreenfootImage bg = new GreenfootImage("background.png");
+        setBackground(bg);
+        
     }
+    
+    public void act()
+       {
+        timecount--;
+        showText(""+timecount, 50,50 );
+        if( timecount == 0 ){
+           showText("TIME OVER", 300, 200);
+           Greenfoot.stop();
+       }  
+        }
 }
--- game4/maou.java
+++ game4/maou.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class A here.
+ * Write a description of class maou here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class A extends Actor
+public class maou extends Actor
 {
     /**
-     * Act - do whatever the A wants to do. This method is called whenever
+     * Act - do whatever the maou wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
