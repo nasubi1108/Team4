146d50c42e3248eed45bed352365c19413793467
nasubi1108 <21097@hakodate.kosen-ac.jp>
2025-12-25 09:17:40 +0900

    修正

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -119,30 +119,30 @@ public class MyWorld extends World
        else if(i==11)
        {
            showText("神:そんなものは待ってられん！！授けた力は長くは続かん！",300,50);
-           showText("",300,50);
+           showText("",300,70);
            i++;
        }
        else if(i==12)
        {
-           showText("神:今ここで魔王を召喚する！！",300,70);
+           showText("神:今ここで魔王を召喚する！！",300,50);
            i++;
        }
        else if(i==13)
        {
-           showText("アノレノレ:えええええええ！？いきなり！？",300,30);
-           showText("というかそんなことできるの！？",300,50);
+           showText("アノレノレ:えええええええ！？いきなり！？",300,50);
+           showText("というかそんなことできるの！？",300,70);
            i++;
        }
        else if(i==14)
        {
-           showText("神:問答無用じゃ！！むむむ...カアアア！！",300,30);
-           showText("",300,50);
+           showText("神:問答無用じゃ！！むむむ...カアアア！！",300,50);
+           showText("",300,70);
            i++;
        }
        else if(i==15)
        {
-           showText("アノレノレ:ちょっと待ってよ！まだ心の準備が！",300,30);
-           showText("うわあああああああああ！！(バトル２突入)",300,50);
+           showText("アノレノレ:ちょっと待ってよ！まだ心の準備が！",300,50);
+           showText("うわあああああああああ！！(バトル２突入)",300,70);
            i++;
        }
        //バトル２突入
@@ -151,8 +151,8 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:やったああああ！！なんとか魔王を倒せたぞ！",300,30);
-           showText("",300,50);
+           showText("アノレノレ:やったああああ！！なんとか魔王を倒せたぞ！",300,50);
+           showText("",300,70);
            i++;
  
        }
@@ -161,7 +161,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神: .....。",300,30);
+           showText("神: .....。",300,50);
  
            i++;
  
@@ -171,7 +171,7 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ: ...？これで世界を救えたんだよね！",300,30);
+           showText("アノレノレ: ...？これで世界を救えたんだよね！",300,50);
  
            i++;
  
@@ -181,7 +181,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神:フフフ......ハハハハハハ！",300,30);
+           showText("神:フフフ......ハハハハハハ！",300,70);
  
            i++;
  
@@ -191,7 +191,8 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:どう...したの？神様...？",300,30);
+           showText("アノレノレ:どう...したの？神様...？",300,50);
+           showText("",300,70);
  
            i++;
  
@@ -201,7 +202,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神:バカめ！まんまと騙されおって！！",300,30);
+           showText("神:バカめ！まんまと騙されおって！！",300,50);
  
            i++;
  
@@ -211,7 +212,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神:これで世界はワシのものじゃあああああああああ！！",300,30);
+           showText("神:これで世界はワシのものじゃあああああああああ！！",300,50);
  
            i++;
  
@@ -221,7 +222,7 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:な...なに...？どういうこと...？",300,30);
+           showText("アノレノレ:な...なに...？どういうこと...？",300,50);
  
            i++;
  
@@ -231,7 +232,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神:はぁ...いいだろう、冥土の土産に教えてやるぞい！",300,30);
+           showText("神:はぁ...いいだろう、冥土の土産に教えてやるぞい！",300,50);
  
            i++;
  
@@ -241,8 +242,8 @@ public class MyWorld extends World
  
        {
  
-           showText("神:ワシはこの素晴らしい世界を支配したかった...",300,30);
-           showText("だが下界には直接干渉できない！",300,50);
+           showText("神:ワシはこの素晴らしい世界を支配したかった...",300,50);
+           showText("だが下界には直接干渉できない！",300,70);
            i++;
  
        }
@@ -251,8 +252,8 @@ public class MyWorld extends World
  
        {
  
-           showText("神:だからお前に最強の力を与え、",300,30);
-           showText("この世界を支配する魔王を倒させたのだ！",300,50);
+           showText("神:だからお前に最強の力を与え、",300,50);
+           showText("この世界を支配する魔王を倒させたのだ！",300,70);
            i++;
  
        }
@@ -261,8 +262,8 @@ public class MyWorld extends World
  
        {
  
-           showText("神:そして今！お前の体を乗っ取り！",300,30);
-           showText("この世界に君臨するのだ！！",300,50);
+           showText("神:そして今！お前の体を乗っ取り！",300,50);
+           showText("この世界に君臨するのだ！！",300,70);
            i++;
  
        }
@@ -271,8 +272,8 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:そんな...！ど、どうしよう、",300,30);
-           showText("ボクは...ボクは...！",300,50);
+           showText("アノレノレ:そんな...！ど、どうしよう、",300,50);
+           showText("ボクは...ボクは...！",300,70);
  
            i++;
  
@@ -282,8 +283,8 @@ public class MyWorld extends World
  
        {
  
-           showText("魔王:...ウ...グッ...貴様の好きにはさせんぞおおお！！",300,30);
-           showText("",300,50);
+           showText("魔王:...ウ...グッ...貴様の好きにはさせんぞおおお！！",300,50);
+           showText("",300,70);
  
            i++;
  
@@ -293,7 +294,7 @@ public class MyWorld extends World
  
        {
  
-           showText("神:魔王！？お主...まだ生きておったか！！",300,30);
+           showText("神:魔王！？お主...まだ生きておったか！！",300,50);
  
            i++;
  
@@ -303,8 +304,8 @@ public class MyWorld extends World
  
        {
  
-           showText("魔王:アノレノレ！貴様に我が闇の力を貸してやる！",300,30);
-           showText("そこの邪神を打ち砕け！",300,50);
+           showText("魔王:アノレノレ！貴様に我が闇の力を貸してやる！",300,50);
+           showText("そこの邪神を打ち砕け！",300,70);
            i++;
  
        }
@@ -313,8 +314,8 @@ public class MyWorld extends World
  
        {
  
-           showText("ア:魔王...！分かった！一緒に行こう！",300,30);
-           showText("〜バトル3突入〜",300,50);
+           showText("ア:魔王...！分かった！一緒に行こう！",300,50);
+           showText("〜バトル3突入〜",300,70);
            i++;
  
        }
@@ -323,8 +324,8 @@ public class MyWorld extends World
  
        {
  
-           showText("神:ぐあああああ！魔力が...！身体が保てない！！",300,30);
-           showText("",300,50);
+           showText("神:ぐあああああ！魔力が...！身体が保てない！！",300,50);
+           showText("",300,70);
            i++;
  
        }
@@ -333,7 +334,7 @@ public class MyWorld extends World
  
        {
  
-           showText("魔王:終わりだ！邪神よ！俺とともに地獄へ墜ちろ！",300,30);
+           showText("魔王:終わりだ！邪神よ！俺とともに地獄へ墜ちろ！",300,50);
  
            i++;
  
@@ -343,7 +344,7 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:魔王！？何をするつもり！？",300,30);
+           showText("アノレノレ:魔王！？何をするつもり！？",300,50);
  
            i++;
  
@@ -353,7 +354,7 @@ public class MyWorld extends World
  
        {
  
-           showText("魔王:アノレノレ...俺の最後の闇の力で神を存在ごと消す！",300,30);
+           showText("魔王:アノレノレ...俺の最後の闇の力で神を存在ごと消す！",300,50);
  
            i++;
  
@@ -363,7 +364,7 @@ public class MyWorld extends World
  
        {
  
-           showText("魔王:このぶよぶよ界を託したぞ！",300,30);
+           showText("魔王:このぶよぶよ界を託したぞ！",300,50);
  
            i++;
  
@@ -373,7 +374,7 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:魔王！う、うわああああああああああ！！",300,30);
+           showText("アノレノレ:魔王！う、うわああああああああああ！！",300,50);
  
            i++;
  
@@ -383,7 +384,7 @@ public class MyWorld extends World
  
        {
  
-           showText("アノレノレ:...ふ、二人とも消えちゃった...",300,30);
+           showText("アノレノレ:...ふ、二人とも消えちゃった...",300,50);
  
            i++;
  
@@ -393,9 +394,9 @@ public class MyWorld extends World
  
        {
  
-           showText("ア:でもこれで世界は平和になったね！",300,30);
+           showText("ア:でもこれで世界は平和になったね！",300,50);
  
-           showText("今日から鍛え直さなきゃ！頑張るぞぉ〜！",300,50);
+           showText("今日から鍛え直さなきゃ！頑張るぞぉ〜！",300,70);
  
            i++;
  
@@ -405,9 +406,9 @@ public class MyWorld extends World
  
        {
  
-           showText("終",300,30);
+           showText("終",300,50);
  
-           showText("",300,50);
+           showText("",300,70);
  
            i++;
  
